d1973fba92a4a16d6e85e5a34ac5e7b77e946e04
rakesh <rakesh.parihar.hs@gmail.com>
2018-10-01 19:05:44 +0530

    - Added Edittext to enter any url - updated gradle files

--- app/src/main/java/com/developer/annasblackhat/chromecustomtabs/MainActivity.java
+++ app/src/main/java/com/developer/annasblackhat/chromecustomtabs/MainActivity.java
@@ -7,33 +7,39 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatEditText edtUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUrl = findViewById(R.id.edtUrl);
     }
 
-    public void btnGo_Click(View v){
-        String url = "https://facebook.com/annas685";
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+    public void btnGo_Click(View v) {
+        String url = edtUrl.getText().toString().trim();
+        if (url.length() > 0) {
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            builder.setToolbarColor(Color.parseColor("#3F51B5"));
+            CustomTabsIntent customTabsIntent = builder.build();
+            CustomTabActivityHelper.openCustomTab(this, customTabsIntent, Uri.parse(url),
+                    new CustomTabActivityHelper.CustomTabFallback() {
+                        @Override
+                        public void openUri(Activity activity, Uri uri) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+                        }
+                    });
+        } else {
+            edtUrl.setError(getString(R.string.hint_enter_website));
+            edtUrl.requestFocus();
+        }
 
-//        CustomTabsIntent customTabsIntent = builder.build();
-//        customTabsIntent.launchUrl(this, Uri.parse(url));
-
-        builder.setToolbarColor(Color.parseColor("#3F51B5"));
-
-        CustomTabsIntent customTabsIntent = builder.build();
-        CustomTabActivityHelper.openCustomTab(this, customTabsIntent, Uri.parse(url),
-                new CustomTabActivityHelper.CustomTabFallback() {
-                    @Override
-                    public void openUri(Activity activity, Uri uri) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
-                    }
-                });
     }
 }
